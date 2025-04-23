53fffbf22df4eee52f8c4d4cf2350966c496cf4f
Leonardo6717 <monteserrat.souza@gmail.com>
2025-04-23 10:44:35 -0300

    config render

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const routes = require('./routes/routes');
 
-// Configurar o CORS
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoURL = process.env.DATABASE_URL; // Agora usando variável de ambiente
+
+// Middleware CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Methods", "HEAD, GET, POST, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 app.use(express.json());
-
-// Porta
-const PORT = process.env.PORT || 3000;
-
-// Rotas
-const routes = require('./routes/routes');
 app.use('/api', routes);
 
-// Conectar ao MongoDB
-const connectionString = process.env.DATABASE_URL || "mongodb+srv://Leonardo237097:LeoGabi261123@cluster0.ou53edn.mongodb.net/tarefasDB?retryWrites=true&w=majority&appName=Cluster0";
+// Conexão com MongoDB
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
 
-mongoose.connect(connectionString)
-  .then(() => {
-    console.log("Database Connected");
+const db = mongoose.connection;
+db.on('error', (error) => console.error('Erro na conexão com o banco:', error));
+db.once('connected', () => console.log('✅ Database Connected'));
 
-    app.listen(PORT, () => {
-      console.log(`Server Started at ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log("Erro na conexão com o MongoDB:", error);
-  });
+// Iniciando servidor
+app.listen(PORT, () => {
+  console.log(`🚀 Server Started at port ${PORT}`);
+});
+
 
